a0669c38ab4b0fdd1a8273badc06a9ea5a092bc9
=Jose Lambrechts <alt165@gmail.com>
2022-05-26 18:03:38 -0300

    estructura principal

---
+++ funciones.py
@@ -0,0 +1,8 @@
+def menu_inicial():
+    seguir = True
+    uno = ""
+    while seguir != False:
+        print("mensaje")
+        uno = input()
+        if uno == "1":
+            seguir = False
---
+++ principal.py
@@ -0,0 +1,3 @@
+import funciones
+
+funciones.menu_inicial() #mostrar el menu de espera
