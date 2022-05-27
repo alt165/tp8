05f9b1fe70b3077de36d8faece410cf0adda8f24
=Jose Lambrechts <alt165@gmail.com>
2022-05-27 15:08:20 -0300

    Prueba funcion clear()

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+import os
 def mensaje_entrada():
     string_bienvenida = """
 
@@ -20,12 +21,19 @@ def menu_inicial():
         print(mensaje_entrada())
         uno = input()
         if uno == "1":
-            seguir = False
+            seguir = False      
+
+
+def clear(): #borrar pantalla
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
 def leer_tarjeta():
     """Esta función devuelve True si la tarjeta es válida
     si no lo es devuelve False"""
-    pass
+    return True
 
 def verificar_clave(clave):
     """Esta función devuelve true si la clave es correcta"""
--- principal.py
+++ principal.py
@@ -1,3 +1,8 @@
 import funciones
 
 funciones.menu_inicial() #mostrar el menu de espera
+funciones.clear()
+print("Inserte su tarjeta")
+
+if leer_tarjeta():
+    
