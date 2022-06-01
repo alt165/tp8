55ab8d9ab09a7ad993d93b9359723af11d34f35d
Luz <luzmtranzillo@gmail.com>
2022-06-01 17:48:46 -0300

    Subida esqueleto principal

--- principal.py
+++ principal.py
@@ -1,8 +1,30 @@
 import funciones
 
-funciones.menu_inicial() #mostrar el menu de espera
-funciones.clear()
-print("Inserte su tarjeta")
-
-if leer_tarjeta():
-    
+def principal():
+        
+    funciones.menu_inicial() #mostrar el menu de espera
+    funciones.clear()
+    print("Inserte su tarjeta")
+    if funciones.leer_tarjeta():#verificar que sea tarjeta valida y devolver los datos
+        pass
+        
+    pedir_dni()
+    pedir_clave()
+    verificar_dni_clave()
+    #si verificar dni y clave se ejecuto tres veces retener tarjeta
+    #else:
+    while seguir:
+        mostrar_menu()
+        #tomar opcion de menu
+        if opcion extraccion:
+            monto
+            funcion_extraccion()
+            funcion_continuar()
+        elif opcion == consultas:
+            funcion_consultas()
+            funcion_continuar()
+        elif opcion == transferencias:
+            funcion_transferencias()
+            funcion_continuar()
+        else:
+            salir()
