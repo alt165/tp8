7ff64539c94f44f3894fc83d4e0c9a85d91a3b48
=Jose Lambrechts <alt165@gmail.com>
2022-06-04 08:33:40 -0300

    agregar fin de linea en logo banco

---
+++ mensajes.py
@@ -0,0 +1,13 @@
+def nombre_interbanca():
+    """Funcíon que devuelve el cartel con el nombre del banco"""
+    interbanca = """
+
+  _____       _            _                           
+ |_   _|     | |          | |                          
+   | |  _ __ | |_ ___ _ __| |__   __ _ _ __   ___ __ _ 
+   | | | '_ \| __/ _ \ '__| '_ \ / _` | '_ \ / __/ _` |
+  _| |_| | | | ||  __/ |  | |_) | (_| | | | | (_| (_| |
+ |_____|_| |_|\__\___|_|  |_.__/ \__,_|_| |_|\___\__,_|
+                                                       
+                                                      \n"""
+    return interbanca
